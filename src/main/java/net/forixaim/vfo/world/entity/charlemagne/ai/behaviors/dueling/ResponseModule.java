39c38826bd49ed3e6e2fee748174891bf90552e4
Matthew <thebest20067@gmail.com>
2024-11-26 08:59:18 -0800

    Fix Jab 2

--- src/main/java/net/forixaim/vfo/world/entity/charlemagne/ai/behaviors/dueling/ResponseModule.java
+++ src/main/java/net/forixaim/vfo/world/entity/charlemagne/ai/behaviors/dueling/ResponseModule.java
@@ -1,5 +1,8 @@
 package net.forixaim.vfo.world.entity.charlemagne.ai.behaviors.dueling;
 
+/**
+ * This module takes in predictions from the Logic Module and responds with appropriate action.
+ */
 public class ResponseModule
 {
 }
